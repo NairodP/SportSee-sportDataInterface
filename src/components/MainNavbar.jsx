05bde97933465e6572b089d404d141217ed4adc3
Dorian <dorianpernot2@gmail.com>
2024-09-11 17:01:42 +0200

    maj icon2

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,13 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
 import "/src/css/components/mainNavbar.css";
 
+import mainLogo from "../assets/img/mainLogo.png";
+
+
 // Composant Navbar affichant la barre de navigation horizontale.
 export default function mainNavbar() {
   return (
       <nav className="nav">
         {/* Logo redirigeant vers la page d'accueil */}
         <Link to="/" className="logo">
-          <img src="/src/assets/img/mainLogo.png" alt="Logo" />
+          <img src={mainLogo} alt="Logo" />
         </Link>
         <ul>
           <li>
